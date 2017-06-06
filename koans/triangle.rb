2696622bc6a5195f8004f3fcec7ed64592d63549
budougumi0617 <yoichiro.shimizu@c-fo.com>
2017-06-06 23:32:23 +0900

    :+1: 155/282

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if a == b && b == c && c == a
+
+  if a == b && b == c && c == a then
+    return :equilateral
+  elsif a != b && b != c && c != a then
+    return :scalene
+  else
+    return :isosceles
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
