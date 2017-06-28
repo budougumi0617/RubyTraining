2d331bfa4b278501560dafae92b32909f82a5fe9
budougumi0617 <budougumi0617@gmail.com>
2017-06-28 21:29:33 +0900

    :+1: 223/282, Add temprature converter.

--- enjoy_ruby/lib/ch12/temp_conv.rb
+++ enjoy_ruby/lib/ch12/temp_conv.rb
@@ -1,5 +1,13 @@
 class TempConv
+    # Return fahrenheit.
     def self.cels_to_fahr c
         c * 9 / 5.to_f + 32
     end
+
+    # Return celsius.
+    def self.fahr_to_cels f
+        ((f - 32) * 5 / 9.to_f).round(4)
+    end
+
+    # 
 end
--- enjoy_ruby/test/ch12/test_temp_conv.rb
+++ enjoy_ruby/test/ch12/test_temp_conv.rb
@@ -6,4 +6,9 @@ class TestTempConv < Test::Unit::TestCase
         assert_equal 32, TempConv.cels_to_fahr(0)
         assert_equal 53.6, TempConv.cels_to_fahr(12)
     end
+
+    def test_fahr_to_cels
+        assert_equal 0, TempConv.fahr_to_cels(32)
+        assert_equal 37.7778, TempConv.fahr_to_cels(100)
+    end
 end
