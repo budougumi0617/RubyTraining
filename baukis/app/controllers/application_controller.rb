f25def2bb268fef9127c21ab0601edd1a22e5c68
budougumi0617 <budougumi0617@gmail.com>
2017-07-21 10:07:08 +0900

    :+1: Add 404 pages -P124

--- baukis/app/controllers/application_controller.rb
+++ baukis/app/controllers/application_controller.rb
@@ -11,23 +11,29 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::RoutingError, with: :rescue404
 
   private
   def set_layout
-      if params[:controller].match(%r{\A(staff|admin|customer)/})
-          Regexp.last_match[1]
-      else
-          'customer'
-      end
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
   end
 
   def rescue403(e)
-      @exception = e
-      render 'errors/forbidden', status: 403
+    @exception = e
+    render 'errors/forbidden', status: 403
+  end
+
+  def rescue404(e)
+    @exception = e
+    render 'errors/not_found', status: 404
   end
 
   def rescue500(e)
-      @exception = e
-      render 'errors/internal_server_error', status: 500
+    @exception = e
+    render 'errors/internal_server_error', status: 500
   end
 end
