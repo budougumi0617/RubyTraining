9accbc982480323144fc6965f419b169fdb1a9e0
budougumi0617 <budougumi0617@gmail.com>
2017-07-26 13:45:33 +0900

    :+1: Add specs for Administrator model.

--- baukis/app/models/administrator.rb
+++ baukis/app/models/administrator.rb
@@ -1,10 +1,10 @@
 class Administrator < ActiveRecord::Base
-  before validation do
-    self.email_for_index = email.downcase if emal
+  before_validation do
+    self.email_for_index = email.downcase if email
   end
 
   def password=(raw_password)
-    if raw_password.kind of?(String)
+    if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil
