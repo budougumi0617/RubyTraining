0e49d55325615c2b234cc9fac0926738a5d85552
budougumi0617 <budougumi0617@gmail.com>
2017-08-04 10:12:05 +0900

    :+1: Modify url paths.

--- baukis/config/routes.rb
+++ baukis/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  namespace :staff, path: '' do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    resource :session, only: [ :create, :destroy ]
-    resource :acount, except: [ :new, :create, :destroy ]
+
+  constraints host: 'baukis.example.com' do
+    namespace :staff, path: '' do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :acount, except: [ :new, :create, :destroy ]
+    end
   end
 
   namespace :admin do
