3270772dac9ceba695237351eeb8564e29c89538
budougumi0617 <budougumi0617@gmail.com>
2017-08-05 13:52:41 +0900

    :+1: Add show method.

--- baukis/app/controllers/admin/staff_members_controller.rb
+++ baukis/app/controllers/admin/staff_members_controller.rb
@@ -2,4 +2,10 @@ class Admin::StaffMembersController < Admin::Base
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
+
+  def show
+    staff_member = StaffMember.find(params[:id]) # id属性値でテーブルからレコードを取得する
+    redirect_to [:edit, :admin, staff_member]
+    # 引数に配列が指定された場合、配列の要素からルーティング名が推定される。
+  end
 end
