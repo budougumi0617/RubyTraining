37ddead784129a07b41ef99c455cc3160fa113c1
budougumi0617 <budougumi0617@gmail.com>
2017-08-11 17:01:18 +0900

    :+1: Add session timeout.

--- baukis/app/controllers/staff/base.rb
+++ baukis/app/controllers/staff/base.rb
@@ -1,6 +1,7 @@
 class Staff::Base < ApplicationController
   before_action :authorize
   before_action :check_account
+  before_action :check_timeout
 
   private
   def authorize
@@ -22,6 +23,20 @@ class Staff::Base < ApplicationController
       session.delete(:staff_member_id)
       flash.alert = 'アカウントが無効になりました。'
       redirect_to :staff_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_staff_member
+      if session[:last_access_time] >= TIMEOUT.ago # 現在からTIMEOUT秒前
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:staff_member_id)
+        flash.alert = 'セッションがタイムアウトしました。'
+        redirect_to :staff_root
+      end
     end
   end
 
