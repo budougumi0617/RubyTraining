a512e29b2c4c77827bb9eab4a4fc568dc8b7e64a
budougumi0617 <budougumi0617@gmail.com>
2017-08-11 17:43:14 +0900

    :+1: Add spec for TopController -P279

---
+++ baukis/spec/controllers/staff/top_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Staff::TopController, 'ログイン後' do
+  let(:staff_member) {create(:staff_member)}
+
+  before do
+    session[:staff_member_id] = staff_member.id
+    session[:last_access_time] = 1.second.ago
+  end
+
+  describe '#index' do
+    example '通常はstaff/indexを表示' do
+      get :index
+      expect(response).to render_template('staff/top/index')
+    end
+
+    example '停止フラグがセットされたら強制的にログアウト' do
+      staff_member.update_column(:suspended, true)
+      get :index
+      expect(session[:staff_member_id]).to be_nil
+      expect(response).to redirect_to(staff_root_url)
+    end
+  end
+end
