9afd38ff5ed958e15a5804b83b6d0bcafcca2c1a
budougumi0617 <budougumi0617@gmail.com>
2017-08-18 09:29:06 +0900

    :+1: Refactoring.

--- baukis/app/controllers/admin/staff_events_controller.rb
+++ baukis/app/controllers/admin/staff_events_controller.rb
@@ -6,6 +6,7 @@ class Admin::StaffEventsController < Admin::Base
     else
       @events = StaffEvent.order(occurred_at: :desc)
     end
+    @events = @events.includes(:member)
     @events = @events.page(params[:page]) # Relationオブジェクトを再検索して@eventsにセット。
   end
 end
