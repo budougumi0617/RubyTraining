1ab616fd37ff23d5bda3afd9afcbd273561219a2
budougumi0617 <budougumi0617@gmail.com>
2017-08-22 00:45:04 +0900

    :+1: Add data.

--- baukis/db/seeds.rb
+++ baukis/db/seeds.rb
@@ -1,4 +1,4 @@
-table_names = %w(staff_members administrators staff_events) # 配列を作るときにカンマなどはいらない。
+table_names = %w(staff_members administrators staff_events customers) # 配列を作るときにカンマなどはいらない。
 table_names.each do |table_name|
   path = Rails.root.join('db', 'seeds', Rails.env, "#{table_name}.rb")
   if File.exist?(path)
