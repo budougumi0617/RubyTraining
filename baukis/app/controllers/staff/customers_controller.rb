a0cb30a7a721482dea600b48d19782dac935bfbd
budougumi0617 <budougumi0617@gmail.com>
2017-08-22 08:44:39 +0900

    :+1: Add show page.

--- baukis/app/controllers/staff/customers_controller.rb
+++ baukis/app/controllers/staff/customers_controller.rb
@@ -4,7 +4,7 @@ class Staff::CustomersController < Staff::Base
       .page(params[:page])
   end
 
-#  def show
-#    @customer = Customer.find(params[:id])
-#  end
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
