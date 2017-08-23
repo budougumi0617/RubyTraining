3a8c45d7511abe68e5ec6f3295646ca78f80f205
budougumi0617 <budougumi0617@gmail.com>
2017-08-23 09:24:31 +0900

    :+1: Add assign_attributes, save methods. -P424

--- baukis/app/forms/staff/customer_form.rb
+++ baukis/app/forms/staff/customer_form.rb
@@ -16,4 +16,50 @@ class Staff::CustomerForm
     # 自分のオブジェクトをインスタンス化する。この段階では
     # データベースに保存されない。
   end
+
+  # 以下の様に利用する。params[:form]は:customerなどのキーをもつハッシュ。
+  # @customer_form.assign_attributes(params[:form])
+  # @paramsにセットしたあとは、プライベートメソッド経由でcustomerにセットしている。
+  # こうすることで、バリデーションを経由することができる。
+  def assign_attributes(params = {})
+    @params = params
+
+    customer.assign_attributes(customer_params)
+    customer.home_address.assign_attributes(home_address_params)
+    customer.work_address.assign_attributes(work_address_params)
+  end
+
+  # 明示的に3つのオブジェクトを保存する。
+  def save
+    ActiveRecord::Base.transaction do # ブロック内をひとつのDB操作として担保できる。
+      customer.save!
+      customer.home_address.save!
+      customer.work_address.save!
+    end
+  end
+
+  private
+
+  # 各バリデーションメソッド。
+  def customer_params
+    @params.require(:customer).permit(
+      :email, :password,
+      :family_name, :given_name, :family_name_kana, :given_name_kana,
+      :birthday, :gender
+    )
+  end
+
+  def home_address_params
+    @params.require(:home_address).permit(
+      :postal_code, :prefecture, :city, :address1, :address2,
+    )
+  end
+
+  def work_address_params
+    @params.require(:work_address).permit(
+      :postal_code, :prefecture, :city, :address1, :address2,
+      :company_name, :division_name
+    )
+  end
+
 end
