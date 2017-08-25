652dd5b214bcd2c81b5ddb6ba7df24a64f77c769
budougumi0617 <budougumi0617@gmail.com>
2017-08-25 09:55:16 +0900

    :+1: Add validation.

--- baukis/app/models/concerns/string_normalizer.rb
+++ baukis/app/models/concerns/string_normalizer.rb
@@ -14,4 +14,11 @@ module StringNormalizer
   def normalize_as_furigana(text)
     NKF.nkf('-W -w -Z1 --katakana', text).strip if text
   end
+
+  # 郵便番号を正規化する。
+  #
+  # @param [string] text 郵便番号
+  def normalize_as_postal_code(text)
+    NKF.nkf('-W -w -Z1', text).strip.gsub(/-/, '') if text
+  end
 end
