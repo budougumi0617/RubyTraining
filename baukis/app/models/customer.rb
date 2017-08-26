9873019a9fc3536ece74cb5c69d21f61daf010f1
budougumi0617 <budougumi0617@gmail.com>
2017-08-26 14:03:02 +0900

    :recycle: Extract EmailHolder, PersonalNameHolder. -P453

--- baukis/app/models/customer.rb
+++ baukis/app/models/customer.rb
@@ -1,11 +1,10 @@
 class Customer < ActiveRecord::Base
+  include EmailHolder
+  include PersonalNameHolder
+
   # autosave: true オブジェクトがDBに保存される際に、関連オブジェクトも自動保存される。
   has_one :home_address, dependent: :destroy, autosave: true # モデル間に1対1の相関関係をつける。
   has_one :work_address, dependent: :destroy, autosave: true # Customerが破棄されるときはその前にAddressも破棄する。
-
-  before_validation do
-    self.email_for_index = email.downcase if email
-  end
 
   # ActiveRecordのモデルに対するバリデーション
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
--- baukis/app/models/staff_member.rb
+++ baukis/app/models/staff_member.rb
@@ -1,26 +1,11 @@
 class StaffMember < ActiveRecord::Base
-  include StringNormalizer
+  include EmailHolder
+  include PersonalNameHolder
+
   # :destroyを指定すると、関連づけられたすべてのStaffEventオブジェクトが、
   # StaffMemberオブジェクトが削除される前に削除される。
   has_many :events, class_name: 'StaffEvent', dependent: :destroy
 
-  before_validation do
-    self.email = normalize_as_email(email)
-    self.email_for_index = email.downcase if email
-    self.family_name = normalize_as_name(family_name)
-    self.given_name = normalize_as_name(given_name)
-    self.family_name_kana = normalize_as_furigana(family_name_kana)
-    self.given_name_kana = normalize_as_furigana(given_name_kana)
-  end
-
-  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}\p{alpha}]+\z/
-  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
-
-  validates :email, presence: true, email: { allow_blank: true }
-  validates :family_name, :given_name, presence: true, # 値が空だと失敗
-    format: { with: HUMAN_NAME_REGEXP, allow_blank: true }
-  validates :family_name_kana, :given_name_kana, presence: true,
-    format: { with: KATAKANA_REGEXP, allow_blank: true } # 値が空の場合はバリデーションをスキップ
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2000, 1, 1), # 指定された日を含む
     # procオブジェクトを作って毎回呼び出さないと、日付が起動時に固定されてしまう。
@@ -32,14 +17,6 @@ class StaffMember < ActiveRecord::Base
     before: ->(obj) { 1.year.from_now.to_date },
     allow_blank: true
   }
-
-  validates :email_for_index, uniqueness: { allow_blank: true }
-  after_validation do
-    if errors.include?(:email_for_index)
-      errors.add(:email, :taken)
-      errors.delete(:email_for_index)
-    end
-  end
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
