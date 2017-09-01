e4092ed70a29eecc6104c08eea35083d6ce320eb
budougumi0617 <budougumi0617@gmail.com>
2017-09-01 09:40:22 +0900

    :+1: Support search phone number, addresses. -P509

--- baukis/app/forms/staff/customer_search_form.rb
+++ baukis/app/forms/staff/customer_search_form.rb
@@ -1,11 +1,14 @@
 class Staff::CustomerSearchForm
   include ActiveModel::Model
+  include StringNormalizer
 
   attr_accessor :family_name_kana, :given_name_kana,
     :birth_year, :birth_month, :birth_mday,
     :address_type, :prefecture, :city, :phone_number
 
   def search
+    normalize_values
+
     # whereメソッドは別のRelationオブジェクトを返すので、条件を追加していく。
     rel = Customer
     if family_name_kana.present?
@@ -18,7 +21,40 @@ class Staff::CustomerSearchForm
     rel = rel.where(birth_month: birth_month) if birth_month.present?
     rel = rel.where(birth_mday: birth_mday) if birth_mday.present?
 
-    # ソート順を決定する。orderメソッドもRelationオブジェクトを返す。
+    # テーブルを結合して、他のテーブルのカラムに基づいてレコードを絞り込む。
+    # 単一テーブル継承で同じaddressテーブルに記録することにしたので、条件が単純で済んでいる。
+    if prefecture.present? || city.present?
+      case address_type
+      when 'home'
+        rel = rel.joins(:home_address)
+      when 'work'
+        rel = rel.joins(:work_address)
+      when ''
+        rel = rel.joins(:addresses)
+      else
+        raise
+      end
+      if prefecture.present?
+        # テーブル名.カラム名
+        rel = rel.where('addresses.prefecture' => prefecture)
+      end
+      rel = rel.where('addresses.city' => city) if city.present?
+    end
+
+    if phone_number.present?
+      rel = rel.joins(:phones).where('phones.number_for_index' => phone_number)
+    end
+
+   # ソート順を決定する。orderメソッドもRelationオブジェクトを返す。
     rel.order(:family_name_kana, :given_name_kana)
+  end
+
+  private
+  def normalize_values
+    self.family_name_kana = normalize_as_furigana(family_name_kana)
+    self.given_name_kana = normalize_as_furigana(given_name_kana)
+    self.city = normalize_as_name(city)
+    self.phone_number = normalize_as_phone_number(phone_number)
+      .try(:gsub, /\D/, '')
   end
 end
--- baukis/app/models/customer.rb
+++ baukis/app/models/customer.rb
@@ -4,8 +4,9 @@ class Customer < ActiveRecord::Base
   include PasswordHolder
 
   # autosave: true オブジェクトがDBに保存される際に、関連オブジェクトも自動保存される。
-  has_one :home_address, dependent: :destroy, autosave: true # モデル間に1対1の相関関係をつける。
-  has_one :work_address, dependent: :destroy, autosave: true # Customerが破棄されるときはその前にAddressも破棄する。
+  has_one :addresses, dependent: :destroy
+  has_one :home_address, autosave: true # モデル間に1対1の相関関係をつける。
+  has_one :work_address, autosave: true # Customerが破棄されるときはその前にAddressも破棄する。
   has_many :phones, dependent: :destroy
   # address_idがnilであることを検索条件に指定している。
   has_many :personal_phones, -> { where(address_id: nil).order(:id) },
