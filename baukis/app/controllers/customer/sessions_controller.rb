92f7c3f6e5c4218728d3ea4cc70a857ad86daf9c
budougumi0617 <budougumi0617@gmail.com>
2017-09-03 15:14:07 +0900

    :+1: Add spec.

--- baukis/app/controllers/customer/sessions_controller.rb
+++ baukis/app/controllers/customer/sessions_controller.rb
@@ -16,7 +16,18 @@ class Customer::SessionsController < Customer::Base
       customer = Customer.find_by(email_for_index: @form.email.downcase)
     end
     if Customer::Authenticator.new(customer).authenticate(@form.password)
-      session[:customer_id] = customer.id
+      if @form.remember_me?
+        # 閲覧不可、変更不可、永続的に情報を残すときは、以下のように記述する。
+        # デフォルト設定では、有効期限は20年。1週間後までにする場合は以下のように書く。
+        # cookies.signed[:customer_id] = {
+        #   value: customer.id,
+        #   expires: 1.week.from_now
+        # }
+        cookies.permanent.signed[:customer_id] = customer.id
+      else
+        cookies.delete(:customer_id)
+        session[:customer_id] = customer.id
+      end
       flash.notice = 'ログインしました。'
       redirect_to :customer_root
     else
@@ -26,6 +37,7 @@ class Customer::SessionsController < Customer::Base
   end
 
   def destroy
+    cookies.delete(:customer_id)
     session.delete(:customer_id)
     flash.notice = 'ログアウトしました。'
     redirect_to :customer_root
