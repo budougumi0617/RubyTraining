8d9420ad8d1925bd79339fb938d4b7665a5455ac
budougumi0617 <budougumi0617@gmail.com>
2017-09-15 09:46:14 +0900

    :+1: Multiply array.

--- enjoy_ruby/lib/ch13/array.rb
+++ enjoy_ruby/lib/ch13/array.rb
@@ -2,4 +2,12 @@ class Array
   def self.sort_array
     (1..100).to_a
   end
+
+  def self.array_times_100(a)
+    a.collect{|i| i * 100 }
+  end
+
+  def self.array_times_100!(a)
+    a.collect!{|i| i * 100 }
+  end
 end
--- enjoy_ruby/test/ch13/test_array.rb
+++ enjoy_ruby/test/ch13/test_array.rb
@@ -8,4 +8,20 @@ class TestArray < Test::Unit::TestCase
       assert_true expect == actual[expect - 1]
     end
   end
+
+  def test_times_100
+    a = (1..100).to_a
+    actual = Array.array_times_100(a)
+    (1..100).each do |i|
+      assert_true i * 100 == actual[i - 1]
+    end
+  end
+
+  def test_times_100!
+    a = (1..10).to_a
+    Array.array_times_100!(a)
+    (1..10).each do |i|
+      assert_true i * 100 == a[i - 1]
+    end
+  end
 end
