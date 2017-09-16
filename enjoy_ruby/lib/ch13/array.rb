ab8b50d083a486dcc4432d842f0fd9bd1469ca37
budougumi0617 <budougumi0617@gmail.com>
2017-09-16 20:46:10 +0900

    :memo: Add comment.

--- enjoy_ruby/lib/ch13/array.rb
+++ enjoy_ruby/lib/ch13/array.rb
@@ -1,12 +1,15 @@
 class Array
+  # 1から100までを順に含んだ配列を返す
   def self.sort_array
     (1..100).to_a
   end
 
+  # 引数配列の各要素を100倍にした配列を返す
   def self.array_times_100(a)
     a.collect{|i| i * 100 }
   end
 
+  # 引数配列の各要素を100倍にする。
   def self.array_times_100!(a)
     a.collect!{|i| i * 100 }
   end
