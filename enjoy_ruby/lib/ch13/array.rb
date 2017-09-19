9cd2f75efafaa756ea9e1b0e341e57fa225f78cb
budougumi0617 <budougumi0617@gmail.com>
2017-09-19 09:50:45 +0900

    :+1: Add sum-array.

--- enjoy_ruby/lib/ch13/array.rb
+++ enjoy_ruby/lib/ch13/array.rb
@@ -23,4 +23,16 @@ class Array
     end
     result
   end
+
+  # 配列の各要素を足し合わせた配列を返す。
+  def sum_array(ary1, ary2)
+    result = Array.new
+    i = 0
+    ary1.each do |elem1|
+      result &lt;&lt; elem1 + ary2[i]
+      i+=1
+    end
+    return result
+  end
+
 end
